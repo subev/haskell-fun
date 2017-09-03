9a021bcd9945600ebcffb28c163139656d857196
Petur Subev <petur.subev@gmail.com>
2017-09-04 00:52:15 +0300

    Add basic monads

---
+++ chapter-18/foo.hs
@@ -0,0 +1,66 @@
+module Foo where
+
+import Control.Monad (join, liftM, liftM2)
+import Control.Applicative (liftA, (*>))
+
+bind :: Monad m => (a -> m b) -> m a -> m b
+bind m g = join $ fmap m g
+
+{-usage is-}
+result1 = (\x -> Just (x + 2)) `bind` (Just 3)
+result2 = (Just 3) >>= (\x -> Just (x + 2))
+
+sequencing :: IO ()
+sequencing = do
+  putStrLn "blah"
+  putStrLn "another thing"
+
+sequencing' :: IO ()
+sequencing' =
+  putStrLn "blah" >>
+  putStrLn "another thing"
+
+sequencing'' :: IO ()
+sequencing'' =
+  putStrLn "blah" *>
+  putStrLn "another thing"
+
+
+binding :: IO ()
+binding = do
+  name <- getLine
+  putStrLn name
+
+binding' :: IO ()
+binding' = do
+  getLine >>= putStrLn
+
+
+bindingAndSequencing :: IO ()
+bindingAndSequencing = do
+  putStrLn "name pls:"
+  name <- getLine
+  putStrLn ("y hello thar: " ++ name)
+
+bindingAndSequencing' :: IO ()
+bindingAndSequencing' =
+  putStrLn "name pls:" >>
+  getLine >>= (\name -> putStrLn ("y helo thar: " ++ name))
+
+twoBinds :: IO ()
+twoBinds = do
+  putStrLn "name pls:"
+  name <- getLine
+  putStrLn "age pls:"
+  age <- getLine
+  putStrLn ("y helo thar: "
+    ++ name ++ " who is: "
+    ++ age ++ " years old.")
+
+twoBinds' :: IO ()
+twoBinds' =
+  putStrLn "name pls:" >>
+  getLine >>= (\name ->
+    putStrLn "age pls:" >>
+    getLine >>= (\age ->
+      putStrLn ("y helo thar: " ++ name ++ "and your age is " ++ age)))
