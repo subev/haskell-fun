b2b8d2ca88439aa2d2fa59a51fe0530312fcf54f
Petur Subev <petur.subev@gmail.com>
2017-09-11 02:32:55 +0300

    Wrap up traversable and foldable exercises

--- chapter-21/laws/app/Main.hs
+++ chapter-21/laws/app/Main.hs
@@ -124,20 +124,154 @@ instance Traversable (List) where
   traverse f (Cons a rest) = liftA2 (Cons) (f a) (traverse f rest)
 
 instance Foldable List where
+  {-TODO cannot make this to work if foldMap below is commented-}
   foldr f d Nil = d
   foldr f d (Cons x rest) = foldr f (f x d) rest
 
+  foldMap ctor (Nil) = mempty
+  foldMap ctor (Cons x rest) = ctor x `mappend` (foldMap ctor rest)
+
+{-ex5-}
+data Three a b c = Three a b c deriving (Eq, Show)
+
+instance Functor (Three a b) where
+  fmap f (Three x y c) = Three x y (f c)
+
+instance (Monoid a, Monoid b) => Applicative (Three a b) where
+  pure x = Three mempty mempty x
+  (<*>) (Three a b c) (Three a' b' c') = Three (a `mappend` a') (b `mappend` b') (c c')
+
+instance (Arbitrary a, Arbitrary b, Arbitrary c) => Arbitrary (Three a b c) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    c <- arbitrary
+    return (Three a b c)
+
+instance Foldable (Three a b) where
+  foldr f d (Three x y c) = f c d
+
+instance Traversable (Three a b) where
+  traverse f (Three x y c) = fmap (Three x y) (f c)
+
+instance (Eq a, Eq b, Eq c) => EqProp (Three a b c) where
+  (=-=) = eq
+
+{-ex6-}
+
+data Three' a b = Three' a b b deriving (Eq, Show)
+
+instance Functor (Three' a) where
+  fmap f (Three' a b c) = Three' a (f b) (f c)
+
+instance (Monoid a) => Applicative (Three' a) where
+  pure x = Three' mempty x x
+  (<*>) (Three' a b c) (Three' a' b' c') = Three' (a `mappend` a') (b b') (c c')
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Three' a b) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    c <- arbitrary
+    return (Three' a b c)
+
+instance (Eq a, Eq b) => EqProp (Three' a b) where
+  (=-=) = eq
+
+instance Foldable (Three' a) where
+  foldr f d (Three' a b b') = f b (f b' d)
+
+instance Traversable (Three' a) where
+  traverse f (Three' a b b') = liftA3 Three' (pure a) (f b) (f b')
+
+{-ex7-}
+
+data S n a = S (n a) a deriving (Eq, Show)
+
+instance (Functor n) => Functor (S n) where
+  fmap f (S n a') = S (fmap f n) (f a')
+
+instance Foldable n => Foldable (S n) where
+  foldMap ctor (S n a') = (foldMap ctor n) `mappend` (ctor a')
+
+instance Traversable n => Traversable (S n) where
+  traverse f (S n a) = liftA2 S (traverse f n) (f a)
+
+instance (Applicative n, Arbitrary a) => Arbitrary (S n a) where
+  arbitrary = do
+    a' <- arbitrary
+    a <- arbitrary
+    return (S (pure a') a)
+
+instance (Eq a) => EqProp (S n a) where
+  (=-=) (S n a) (S n' a') = eq a a'
+
+{-ex8-}
+data Tree a = Empty | Leaf a | Node (Tree a) a (Tree a) deriving (Eq, Show)
+
+instance (Arbitrary a) => Arbitrary (Tree a) where
+  arbitrary = frequency [
+      (1, return Empty),
+      (2, liftA3 Node arbitrary arbitrary arbitrary),
+      (2, fmap Leaf arbitrary)
+    ]
+
+instance (Eq a) => EqProp (Tree a) where
+  (=-=) = eq
+
+instance Functor Tree where
+  fmap f (Empty) = Empty
+  fmap f (Leaf a) = Leaf (f a)
+  fmap f (Node t x t') = Node (fmap f t) (f x) (fmap f t')
+
+instance Foldable Tree where
+  foldMap ctor (Empty) = mempty
+  foldMap ctor (Leaf a) = ctor a
+  foldMap ctor (Node t a t') = (foldMap ctor t) `mappend` (ctor a) `mappend` (foldMap ctor t')
+
+instance Traversable Tree where
+  traverse f Empty = pure Empty
+  traverse f (Leaf a) = fmap Leaf (f a)
+  traverse f (Node t a t') = liftA3 Node (traverse f t) (f a) (traverse f t')
+
+{-main-}
 main :: IO ()
 main = do
+  putStrLn "<<<<<Alias of []>>>>>"
   let trigger = undefined :: TI (Int, Int, [Int])
   quickBatch (traversable trigger)
+
+  putStrLn "<<<<<Identity>>>>>"
   let identityTrigger = undefined :: Identity (Int, Int, [Int])
   quickBatch (traversable identityTrigger)
+
+  putStrLn "<<<<<Constant>>>>>"
   let constantTrigger = undefined :: Constant Int (Int, Int, [Int])
   quickBatch (traversable constantTrigger)
+
+  putStrLn "<<<<<Maybe>>>>>"
   let maybeTrigger = undefined :: Optional (Int, Int, [Int])
   quickBatch (traversable maybeTrigger)
-  {-TODO THIS TEST DOES NOT PASS :((-}
+
+  putStrLn "<<<<<List>>>>>"
   let listTrigger = undefined :: List (Int, Int, [Int])
+  quickBatch $ applicative (undefined :: List (String, Int, Char))
   quickBatch (traversable listTrigger)
+
+  putStrLn "<<<<<Three>>>>>"
+  let threeTrigger = undefined :: Three Int Int (Int, Int, [Int])
+  quickBatch (traversable threeTrigger)
+
+  putStrLn "<<<<<Three'>>>>>"
+  let threeTrigger' = undefined :: Three' Int (Int, Int, [Int])
+  quickBatch (traversable threeTrigger')
+
+  putStrLn "<<<<<SuckOne>>>>>"
+  let suckTrigger = undefined :: S (Maybe) (Int, Int, [Int])
+  quickBatch (traversable suckTrigger)
+
+  putStrLn "<<<<<Tree>>>>>"
+  let treeTrigger = undefined :: Tree (Int, Int, [Int])
+  quickBatch (functor treeTrigger)
+  quickBatch (traversable treeTrigger)
 
